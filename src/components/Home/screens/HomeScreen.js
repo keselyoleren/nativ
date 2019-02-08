3d723b96bba56e9a901b2dfd8be0597f327dd109
lerenyokesel <lerenyokesel@gmail.com>
2019-02-08 19:05:43 +0700

    '

---
+++ src/components/Home/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View,
+    Button
+ } 
+from 'react-native';
+
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="go to login" onPress={() => this.props.navigation.navigate('Login')}></Button>
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
